7a7d2c48d492aa9093a17d5db81274f9cde691e0
maryaslanyann <mary.aslanyann.06@gmail.com>
2025-10-08 18:36:28 +0400

    Update task_3_Timer_Bonus_Project.py

--- task_3_Timer_Bonus_Project.py
+++ task_3_Timer_Bonus_Project.py
@@ -39,6 +39,30 @@ while int(hour) > -1:
         hour-=1
         
         minute = 59
+
+    else:
+        for i in range(int(minute),-1,-1):
+            for j in range(int(second),-1,-1):
+                if i < 10:
+                    if j < 10:
+                       print(f"{int(hour)}:0{i}:0{j}")
+                    else:
+                        print(f"{int(hour)}:0{i}:{j}")
+                else:
+                    if j < 10:
+                        print(f"{int(hour)}:{i}:0{j}")
+                    else:
+                        print(f"{int(hour)}:{i}:{j}")
+
+                t.sleep(1)
+
+               
+            second = 59
+
+        hour = int(hour)
+        hour-=1
+        
+        minute = 59
          
 
 print("The End :)")
